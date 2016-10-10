90f20f755cb39ff339931bb48caf5680a02eb486
dgellow <samuel.elborai@gmail.com>
2016-10-10 13:49:38 +0200

    buttons for Pad loop

--- Assets/Scripts/Elements/PadLoop.cs
+++ Assets/Scripts/Elements/PadLoop.cs
@@ -1,17 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum LoopState {
+	Play,
+	Pause,
+	Stop
+}
+
 [RequireComponent(typeof(SourceElement))]
 public class PadLoop : MonoBehaviour, ICanSendAudio {
 
 	public float sleepTime = 2f;
 	public AudioClip sample;
+	public LoopState loopState = LoopState.Stop;
+	public SpriteRenderer buttonPlaySprite;
+	public SpriteRenderer buttonPauseSprite;
+	public SpriteRenderer buttonStopSprite;
+	public Color buttonDisableColor;
+	public Color buttonDefaultColor;
 
 	SourceElement sourceElement;
+	Coroutine innerLoop;
 
 	void Start () {
 		sourceElement = GetComponent<SourceElement> ();
-		StartCoroutine (PlayLoop());
+	}
+
+	void OnGUI() {
+		buttonPlaySprite.color = buttonDefaultColor;
+		buttonPauseSprite.color = buttonDefaultColor;
+		buttonStopSprite.color = buttonDefaultColor;
+
+		if (loopState == LoopState.Play) {
+			buttonPlaySprite.color = buttonDisableColor;
+		} else if (loopState == LoopState.Pause) {
+			buttonPauseSprite.color = buttonDisableColor;
+		} else if (loopState == LoopState.Stop) {
+			buttonPauseSprite.color = buttonDisableColor;
+			buttonStopSprite.color = buttonDisableColor;
+		}
+	}
+
+	void Update() {
+		if (innerLoop == null && loopState == LoopState.Play) {
+			innerLoop = StartCoroutine (PlayInnerLoop ());
+		} else if (innerLoop != null && loopState == LoopState.Stop) {
+			StopCoroutine (innerLoop);
+			innerLoop = null;
+		}
 	}
 
 	#region ICanSendAudio implementation
@@ -22,11 +58,27 @@ public class PadLoop : MonoBehaviour, ICanSendAudio {
 
 	#endregion
 
-	IEnumerator PlayLoop() {
-		while (true) {
-			Debug.Log (string.Format("Tick PadLoop.PlayLoop #{0}", GetHashCode ()));			
-			AudioController.SendEvent (sourceElement);
-			yield return new WaitForSeconds (sleepTime);
+	public void SetLoopStatePlay() {
+		loopState = LoopState.Play;
+	}
+
+	public void SetLoopStatePause() {
+		loopState = LoopState.Pause;
+	}
+
+	public void SetLoopStateStop() {
+		loopState = LoopState.Stop;
+	}
+
+	IEnumerator PlayInnerLoop() {
+		while (loopState != LoopState.Stop) {
+			Debug.Log (string.Format("Tick PadLoop.PlayLoop #{0}", GetHashCode ()));
+			if (loopState == LoopState.Pause) {
+				yield return new WaitWhile (() => loopState == LoopState.Pause);
+			} else if (loopState == LoopState.Play) {
+				AudioController.SendEvent (sourceElement);
+				yield return new WaitForSeconds (sleepTime);	
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/Elements/PadLoopButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class PadLoopButton : MonoBehaviour {
+	public UnityEvent onClick;
+
+	#if UNITY_IOS
+
+	Collider2D collider;
+
+	void Awake() {
+		collider = GetComponent<Collider2D> ();
+	}
+		
+	void Update() {
+		if (Utilities.CheckTouch (collider, Camera.main)) {
+			onClick.Invoke ();
+		}
+	}
+
+	#endif
+
+	#if UNITY_EDITOR
+
+	void OnMouseDown() {
+	    onClick.Invoke ();
+	}
+
+	#endif
+}
